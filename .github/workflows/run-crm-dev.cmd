d4faca9081b942cb6db83002d9e258bf4b230234
joelsmeby <joelsmeby@gmail.com>
2026-05-17 07:45:46 -0700

    Delete .github/workflows directory

--- .github/workflows/run-crm-dev.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set "ROOT=%~dp0"
-set "CRM_ROOT=%ROOT%windy-crm"
-set "NODE_DIR=%ROOT%.tools\node-v24.14.0-win-x64"
-set "PATH=%NODE_DIR%;%NODE_DIR%\node_modules\npm\bin;%PATH%"
-cd /d "%CRM_ROOT%"
-"%NODE_DIR%\node.exe" "%NODE_DIR%\node_modules\npm\bin\npm-cli.js" run dev
